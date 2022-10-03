fd5e002a5ae2a9e7499e2dc200d889308ee709b0
jado <j@jadon.io>
2022-10-03 16:32:45 -0700

    project 2: fill in some v3 definitions

--- project2/v3math.c
+++ project2/v3math.c
@@ -3,29 +3,40 @@
  * Authors: Jadon Fowler, Harshith Shakelli
  */
 #include "v3math.h"
+#include <math.h>
 
 void v3_from_points(float *dst, float *a, float *b) {
-
+    dst[0] = a[0] - b[0];
+    dst[1] = a[1] - b[1];
+    dst[2] = a[2] - b[2];
 }
 
 void v3_add(float *dst, float *a, float *b) {
-
+    dst[0] = a[0] + b[0];
+    dst[1] = a[1] + b[1];
+    dst[2] = a[2] + b[2];
 }
 
 void v3_subtract(float *dst, float *a, float *b) {
-
+    dst[0] = a[0] - b[0];
+    dst[1] = a[1] - b[1];
+    dst[2] = a[2] - b[2];
 }
 
 float v3_dot_product(float *a, float *b) {
-    return 0;
+    return a[0] * b[0] + a[1] * b[1] + a[2] * b[2];
 }
 
 void v3_cross_product(float *dst, float *a, float *b) {
-
+    dst[0] = a[1] * b[2] - a[2] * b[1];
+    dst[1] = a[2] * b[0] - a[0] * b[2];
+    dst[2] = a[0] * b[1] - a[1] * b[0];
 }
 
 void v3_scale(float *dst, float s) {
-
+    dst[0] = dst[0] * s;
+    dst[1] = dst[1] * s;
+    dst[2] = dst[2] * s;
 }
 
 float v3_angle(float *a, float *b) {
@@ -48,6 +59,12 @@ void v3_normalize(float *dst, float *a) {
 
 }
 
+bool almostEqual(double a, double b, float tolerance) {
+    return fabs(a - b) < tolerance;
+}
+
 bool v3_equals(float *a, float *b, float tolerance) {
-    return false;
+    return almostEqual(a[0], b[0], tolerance) &&
+           almostEqual(a[1], b[1], tolerance) &&
+           almostEqual(a[2], b[2], tolerance);
 }
