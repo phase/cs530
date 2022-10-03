85d33eab9308e4a64152fb4073ea0d651093a138
jado <j@jadon.io>
2022-10-03 16:07:38 -0700

    project 2: create function signatures

---
+++ project2/v3math.c
@@ -0,0 +1,53 @@
+/**
+ * CS 530 - Project 2
+ * Authors: Jadon Fowler, Harshith Shakelli
+ */
+#include "v3math.h"
+
+void v3_from_points(float *dst, float *a, float *b) {
+
+}
+
+void v3_add(float *dst, float *a, float *b) {
+
+}
+
+void v3_subtract(float *dst, float *a, float *b) {
+
+}
+
+float v3_dot_product(float *a, float *b) {
+    return 0;
+}
+
+void v3_cross_product(float *dst, float *a, float *b) {
+
+}
+
+void v3_scale(float *dst, float s) {
+
+}
+
+float v3_angle(float *a, float *b) {
+    return 0;
+}
+
+float v3_angle_quick(float *a, float *b) {
+    return 0;
+}
+
+void v3_reflect(float *dst, float *v, float *n) {
+
+}
+
+float v3_length(float *a) {
+    return 0;
+}
+
+void v3_normalize(float *dst, float *a) {
+
+}
+
+bool v3_equals(float *a, float *b, float tolerance) {
+    return false;
+}
---
+++ project2/v3test.c
@@ -0,0 +1,8 @@
+/**
+ * CS 530 - Project 2
+ * Authors: Jadon Fowler, Harshith Shakelli
+ */
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
