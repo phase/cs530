37b7984e5a893dcd1cb85622017fc035ada9155f
jado <j@jadon.io>
2022-10-03 21:55:22 -0700

    project 2: add some 4 element vector functions

--- project2/v3math.c
+++ project2/v3math.c
@@ -11,16 +11,37 @@ void v3_from_points(float *dst, float *a, float *b) {
     dst[2] = a[2] - b[2];
 }
 
+void v4_from_points(float *dst, float *a, float *b) {
+    dst[0] = a[0] - b[0];
+    dst[1] = a[1] - b[1];
+    dst[2] = a[2] - b[2];
+    dst[3] = a[3] - b[3];
+}
+
 void v3_add(float *dst, float *a, float *b) {
     dst[0] = a[0] + b[0];
     dst[1] = a[1] + b[1];
     dst[2] = a[2] + b[2];
 }
 
+void v4_add(float *dst, float *a, float *b) {
+    dst[0] = a[0] + b[0];
+    dst[1] = a[1] + b[1];
+    dst[2] = a[2] + b[2];
+    dst[3] = a[3] + b[3];
+}
+
 void v3_subtract(float *dst, float *a, float *b) {
     dst[0] = a[0] - b[0];
     dst[1] = a[1] - b[1];
     dst[2] = a[2] - b[2];
+}
+
+void v4_subtract(float *dst, float *a, float *b) {
+    dst[0] = a[0] - b[0];
+    dst[1] = a[1] - b[1];
+    dst[2] = a[2] - b[2];
+    dst[3] = a[3] - b[3];
 }
 
 float v3_dot_product(float *a, float *b) {
@@ -37,6 +58,13 @@ void v3_scale(float *dst, float s) {
     dst[0] = dst[0] * s;
     dst[1] = dst[1] * s;
     dst[2] = dst[2] * s;
+}
+
+void v4_scale(float *dst, float s) {
+    dst[0] = dst[0] * s;
+    dst[1] = dst[1] * s;
+    dst[2] = dst[2] * s;
+    dst[3] = dst[3] * s;
 }
 
 float v3_angle(float *a, float *b) {
@@ -67,4 +95,11 @@ bool v3_equals(float *a, float *b, float tolerance) {
     return almostEqual(a[0], b[0], tolerance) &&
            almostEqual(a[1], b[1], tolerance) &&
            almostEqual(a[2], b[2], tolerance);
+}
+
+bool v4_equals(float *a, float *b, float tolerance) {
+    return almostEqual(a[0], b[0], tolerance) &&
+           almostEqual(a[1], b[1], tolerance) &&
+           almostEqual(a[2], b[2], tolerance) &&
+           almostEqual(a[3], b[3], tolerance);
 }
