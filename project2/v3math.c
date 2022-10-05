1b0d2ecca0829443fd64c95cf6491489f54a52dc
Harhon <30802005+gohan9959@users.noreply.github.com>
2022-10-04 17:31:17 -0700

    Update v3math.c

--- project2/v3math.c
+++ project2/v3math.c
@@ -40,11 +40,18 @@ void v3_scale(float *dst, float s) {
 }
 
 float v3_angle(float *a, float *b) {
-    return 0;
+    float PI = 3.14;
+    float *a_normal, *b_normal;
+    v3_normalize(a_normal, a);
+    v3_normalize(b_normal, b);
+    return acos(v3_dot_product(a_normal, b_normal))*180/PI;
 }
 
 float v3_angle_quick(float *a, float *b) {
-    return 0;
+    float *a_normal, *b_normal;
+    v3_normalize(a_normal, a);
+    v3_normalize(b_normal, b);
+    return v3_dot_product(a_normal, b_normal);
 }
 
 void v3_reflect(float *dst, float *v, float *n) {
@@ -52,11 +59,14 @@ void v3_reflect(float *dst, float *v, float *n) {
 }
 
 float v3_length(float *a) {
-    return 0;
+    return sqrt(pow(a[0], 2) + pow(a[1], 2) + pow(a[2], 2));
 }
 
 void v3_normalize(float *dst, float *a) {
-
+    float len = v3_length(a);
+    dst[0] = a[0]/len;
+    dst[1] = a[1]/len;
+    dst[2] = a[2]/len;
 }
 
 bool almostEqual(double a, double b, float tolerance) {
