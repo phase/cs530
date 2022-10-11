df11906d79cacdb52d205379a1c2605eb3583ed5
Harhon <30802005+gohan9959@users.noreply.github.com>
2022-10-10 20:30:29 -0700

    Project3

---
+++ project3/main.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "v3math.h"
+
+typedef struct{
+    sphere *Sphere;
+    plane *Plane;
+} object;
+
+typedef struct{
+    char * type;
+    float color[3];
+    int position[3];
+    int radius;
+} sphere;
+
+typedef struct{
+    char * type;
+    float color[3];
+    int position[3];
+    int normal[3];
+} plane;
+
+object *readFile(char *filename){
+    // read File
+}
+
+int *createPixArray(object *Objects){
+    // convert objects and put them in int array.
+}
+
+void writePixArray(int *pixArray){
+    // Write the pix array data to P3 file.
+}
+
+int *raycast(object *Objects){
+    // call appropriate function to cast ray.
+}
+
+int *raycastSphere(int *pixArray, sphere *Sphere){
+    //Cast a ray to a sphere.
+}
+
+int *raycastPlane(int *pixArray, plane *Plane){
+    //Cast a ray to a plane.
+}
+
+int main(int argc, char *argv[]){
+    return 0;
+}
