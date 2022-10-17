0ec63bd21a942450da9ddc91893cd1390f540627
Harhon <30802005+gohan9959@users.noreply.github.com>
2022-10-16 23:09:39 -0700

    Implemented readFile

--- project3/main.c
+++ project3/main.c
@@ -7,26 +7,157 @@
 #include "v3math.h"
 
 typedef struct{
-    sphere *Sphere;
-    plane *Plane;
-} object;
-
-typedef struct{
-    char * type;
     float color[3];
     int position[3];
     int radius;
 } sphere;
 
 typedef struct{
-    char * type;
     float color[3];
     int position[3];
     int normal[3];
 } plane;
 
+typedef struct{
+    int flag; //if flag = 0 then it is a sphere, flag = 1 then it is a plane.
+    sphere *Sphere;
+    plane *Plane;
+} object;
+
+
+FILE *extractData(FILE * fh, char *ch, char *str){
+    int count = 0;
+    *ch = fgetc(fh);
+    while (*ch != ',' && *ch != ':' && *ch != ' ' && *ch != EOF && *ch !='[' && *ch != ']' && *ch != '\n'){
+        //printf("%c", *ch);
+        str[count] = *ch;
+        count++;
+        *ch = fgetc(fh);
+    }
+    //printf("%c", *ch);
+    if(count > 0)
+        str[count] = '\0';
+    else
+        str[0] = '\0';
+    return fh;
+}
+
 object *readFile(char *filename){
-    // read File
+    // read the json file
+    FILE *fh = fopen(filename, "r");
+    char ch = ' ';
+    char *str;
+    int size = 0;
+    int count;
+    object *Objects = malloc(sizeof(Objects) * 128);
+    while(ch != EOF){
+        str = malloc(sizeof(char) * 20);
+        fh = extractData(fh, &ch, str);
+        if(strcmp(str, "sphere") == 0){
+            Objects[size].flag = 0;
+            Objects[size].Sphere = malloc(sizeof(sphere));
+            while(ch != '\n' && ch != EOF){
+                str = malloc(sizeof(char) * 20);
+                fh = extractData(fh, &ch, str);
+                if(strcmp(str, "color") == 0){
+                    count = 0;
+                    while (true){
+                        str = malloc(sizeof(char) * 8);
+                        fh = extractData(fh, &ch, str);
+                        if(strlen(str) > 0){
+                            printf("%s\n", str);
+                            Objects[size].Sphere->color[count] = atof(str);
+                            count++;
+                        }
+                        if(count == 3){
+                            break;
+                        }
+                    } 
+                }
+                else if (strcmp(str, "position") == 0){
+                    count = 0;
+                    while (true){
+                        str = malloc(sizeof(char) * 8);
+                        fh = extractData(fh, &ch, str);
+                        if(strlen(str) > 0){
+                            printf("%s\n", str);
+                            Objects[size].Sphere->position[count] = atof(str);
+                            count++;
+                        }
+                        if(count == 3){
+                            break;
+                        }
+                    }
+                }
+                else if (strcmp(str, "radius") == 0){
+                    fgetc(fh);
+                    fh = extractData(fh, &ch, str);
+                    printf("%s\n", str);
+                    Objects[size].Sphere->radius = atoi(str);
+                }
+                
+            }
+            size++;
+        }
+        if(strcmp(str, "plane") == 0){
+            Objects[size].flag = 1;
+            Objects[size].Plane = malloc(sizeof(plane));
+            while(ch != '\n' && ch != EOF){
+                str = malloc(sizeof(char) * 20);
+                fh = extractData(fh, &ch, str);
+                if(strcmp(str, "color") == 0){
+                    count = 0;
+                    while (true){
+                        str = malloc(sizeof(char) * 8);
+                        fh = extractData(fh, &ch, str);
+                        if(strlen(str) > 0){
+                            printf("%s\n", str);
+                            Objects[size].Plane->color[count] = atof(str);
+                            count++;
+                        }
+                        if(count == 3){
+                            break;
+                        }
+                    } 
+                }
+                else if (strcmp(str, "position") == 0){
+                    count = 0;
+                    while (true){
+                        str = malloc(sizeof(char) * 8);
+                        fh = extractData(fh, &ch, str);
+                        if(strlen(str) > 0){
+                            printf("%s\n", str);
+                            Objects[size].Plane->position[count] = atof(str);
+                            count++;
+                        }
+                        if(count == 3){
+                            break;
+                        }
+                    }
+                }
+                else if (strcmp(str, "normal") == 0){
+                    count = 0;
+                    while (true){
+                        str = malloc(sizeof(char) * 8);
+                        fh = extractData(fh, &ch, str);
+                        if(strlen(str) > 0){
+                            printf("%s\n", str);
+                            Objects[size].Plane->normal[count] = atof(str);
+                            count++;
+                        }
+                        if(count == 3){
+                            break;
+                        }
+                    }
+                }
+                
+            }
+            size++;
+        } 
+    }
+    printf("\nSphere - Radius: %d, Color: [%f, %f, %f], Position: [%d, %d, %d]", Objects[0].Sphere->radius, Objects[0].Sphere->color[0], Objects[0].Sphere->color[1], Objects[0].Sphere->color[2], Objects[0].Sphere->position[0], Objects[0].Sphere->position[1], Objects[0].Sphere->position[2]);
+    printf("\nPlane - Normal: [%d, %d, %d], Color: [%f, %f, %f], Position: [%d, %d, %d]", Objects[1].Plane->normal[0], Objects[1].Plane->normal[1], Objects[1].Plane->normal[2], Objects[1].Plane->color[0], Objects[1].Plane->color[1], Objects[1].Plane->color[2], Objects[1].Plane->position[0], Objects[1].Plane->position[1], Objects[1].Plane->position[2]);
+    return NULL;
 }
 
 int *createPixArray(object *Objects){
@@ -54,5 +185,6 @@ int *drawRay(int *pixArray, int *startPoint, int *endPoint){
 }
 
 int main(int argc, char *argv[]){
+    readFile(argv[1]);
     return 0;
 }
