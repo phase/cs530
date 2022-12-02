2bc9496e741726a56412f4b48f01485421a0a8f0
jado <j@jadon.io>
2022-12-01 21:29:59 -0700

    project 6: add lerping

--- project6/image_viewer.js
+++ project6/image_viewer.js
@@ -4,59 +4,75 @@ function start(image) {
 
     var canvas = document.getElementById("webgl_example");
     var gl = canvas.getContext("webgl");
-    var scale = 1.0
-    var angle = 0.0
-    var shrx = 0.0
-    var shry = 0.0
-    var xcoordinate = 0.0
-    var ycoordinate = 0.0
+    var scale = 1.0, s2 = 1.0
+    var angle = 0.0, a2 = 0.0
+    var shrx = 0.0, shrx2 = 0.0
+    var shry = 0.0, shry2 = 0.0
+    var xcoordinate = 0.0, x2 = 0.0
+    var ycoordinate = 0.0, y2 = 0.0
+    var timer = 0
     document.addEventListener('keydown', (event) => {
         if (event.key == 'ArrowRight') {
-            xcoordinate += 0.1
+            x2 = xcoordinate + 0.1
+            timer = 0
         }
         if (event.key == 'ArrowLeft') {
-            xcoordinate -= 0.1
+            x2 = xcoordinate - 0.1
+            timer = 0
         }
         if (event.key == 'ArrowUp') {
-            ycoordinate += 0.1
+            y2 = ycoordinate + 0.1
+            timer = 0
         }
         if (event.key == 'ArrowDown') {
-            ycoordinate -= 0.1
+            y2 = ycoordinate - 0.1
+            timer = 0
         }
         if (event.key == 's') {
-            scale -= 0.1
+            s2 = scale - 0.1
+            timer = 0
         }
         if (event.key == 'S') {
-            scale += 0.1
+            s2 = scale + 0.1
+            timer = 0
         }
         if (event.key == '3') {
-            scale *= 2.0
+            s2 = scale * 2.0
+            timer = 0
         }
         if (event.key == '4') {
-            scale *= 0.5
+            s2 = scale * 0.5
+            timer = 0
         }
         if (event.key == 'r') {
-            angle -= 0.1
+            a2 = angle - 0.1
+            timer = 0
         }
         if (event.key == 'R') {
-            angle += 0.1
+            a2 = angle + 0.1
+            timer = 0
         }
         if (event.key == '1') {
-            angle -= 1.5708
+            a2 = angle - 1.5708
+            timer = 0
         }
         if (event.key == '2') {
-            angle += 1.5708
+            a2 = angle + 1.5708
+            timer = 0
         }
         if (event.key == 'r') {
-            angle -= 0.1
+            a2 = angle - 0.1
+            timer = 0
         }
         if (event.key == 'h') {
-            shrx += 0.1
-            shry += 0.1
+            shrx2 = shrx + 0.1
+            shry2 = shry + 0.1
+            timer = 0
         }
         if (event.key == 'H') {
-            shrx -= 0.1
-            shry -= 0.1
+            shrx2 = shrx - 0.1
+            shry2 = shry - 0.1
+            timer = 0
         }
     });
     // 1. Specify geometry / coordinates
@@ -180,8 +196,16 @@ function start(image) {
 
     //    # 10. Create the render loop
 
+    function lerp(a, b, t) { return a + (b - a) * t; }
+
     function draw(t) {
         t /= 500;
+        xcoordinate = lerp(xcoordinate, x2, timer);
+        ycoordinate = lerp(ycoordinate, y2, timer);
+        scale = lerp(scale, s2, timer);
+        angle = lerp(angle, a2, timer);
+        shrx = lerp(shrx, shrx2, timer);
+        shry = lerp(shry, shry2, timer);
         gl.uniformMatrix4fv(
             gl.getUniformLocation(shader_program, "translation"), false, ([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, xcoordinate, ycoordinate, 0, 1])
         );
@@ -194,6 +218,8 @@ function start(image) {
         gl.uniformMatrix4fv(
             gl.getUniformLocation(shader_program, "sheer"), false, ([1, shrx, 0, 0, shry, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1])
         );
+
+        timer = Math.min(1.0, timer + 0.1);
 
         // 11. Render
 
